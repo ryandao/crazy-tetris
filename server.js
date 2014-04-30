96dafc7370192315aaedfcf944eac6feca6c3664
Ryan Dao <daoduyducduong@gmail.com>
2014-04-30 19:27:33 +0800

    Minor update for static server.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Static server to serve the client demo
+var port = 8000;
 var connect = require('connect');
-connect().use(connect.static('client')).listen(8000);
+connect().use(connect.static('client')).listen(port);
 
